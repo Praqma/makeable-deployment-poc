4ceb0b36e81e2480580fbf9099f4d88d87e3bc28
hoeghh <heh@praqma.net>
2015-12-08 12:12:33 +0100

    First milestone. Apache PHP and MySql and Datacontainer pulling Git repository PHP code specified in variable

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # makeable
 POC for makeable
+This script starts a PHP and mysql in Docker and links them.
+It then pulls a repository specified in the docker-compose yml file.
+
+Right now you can access http://localhost:80 and http://localhost:80/testmysql.php
+It pulls the repository : https://github.com/Praqma/makeable-code
+
+# To run:
+./install.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 # Create datacontainer to house our website
 (cd images/data/context
-docker build -t makeable/datacontainer .)
+docker build -t makeable/datacontainer .
+docker create --name datacontainer -t makeable/datacontainer:latest .)
 
 # Build our Apache PHP container
 (cd images/php/context
-docker build -t makeable/php .)
+docker build -t makeable/php:latest .)
+
+# Build our MySql database container
+(cd images/mysql/context
+docker build -t makable/mysql:latest .)
+
+# Start docker-compose
+(cd compose
+docker-compose -f ./makeable-dev.yml up)
