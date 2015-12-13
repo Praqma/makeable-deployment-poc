702f609757c220eceb3b3e8a74cbe7c5f834bddb
Johan Abildskov <joa@praqma.net>
2015-12-13 22:16:18 +0100

    Added vim image to developer. Added environment variables to distinguish prod/dev. Now tests for directory existence and pulls in that, rather than trying to always clone

--- images/php/context/startup.sh
+++ images/php/context/startup.sh
@@ -1,3 +1,7 @@
 echo "Cloning repo : $makeable_repo"
-git clone $makeable_repo /var/www/site
+if [ ! -d /var/www/site ]; then
+  git clone $makeable_repo /var/www/site
+fi
+cd /var/www/site
+git pull
 /usr/sbin/apache2ctl -D FOREGROUND
